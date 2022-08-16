c9ae079ce7849997d336c2cbf3e2041a2a84f4f2
najibdj27 <najib.djulfikar@gmail.com>
2022-08-16 22:08:14 +0700

    initiate commit

---
+++ src/main/java/com/unper/samper/SamperApplication.java
@@ -0,0 +1,13 @@
+package com.unper.samper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamperApplication.class, args);
+	}
+
+}
