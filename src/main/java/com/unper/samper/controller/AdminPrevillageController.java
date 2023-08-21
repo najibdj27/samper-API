afef0fdd38b2ab85494bdbed009d6f8f0ac73801
najibdj_ <najib.djulfikar@gmail.com>
2023-08-21 16:54:15 +0700

    add authorization for getallbycurrentadmin

--- src/main/java/com/unper/samper/controller/AdminPrevillageController.java
+++ src/main/java/com/unper/samper/controller/AdminPrevillageController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ public class AdminPrevillageController {
     AdminPrevillageServiceImpl adminPrevillageServiceImpl;
     
     @GetMapping("/getbycurrentadmin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<?> getAllByCurrentAdmin() throws ResourceNotFoundException {
         List<Previllage> previllageList = adminPrevillageServiceImpl.getAllByCurrentAdmin();
 
