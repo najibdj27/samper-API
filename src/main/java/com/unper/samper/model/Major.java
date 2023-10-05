a199672a62c308e12ddcdcb219a9889f54d5dc51
NAJIB DJULFIKAR <najib.djulfikar@gmail.com>
2023-10-05 22:32:06 +0700

    create major model

---
+++ src/main/java/com/unper/samper/model/Major.java
@@ -0,0 +1,52 @@
+package com.unper.samper.model;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.unper.samper.model.common.Audit;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "major", schema = "public", uniqueConstraints = {@UniqueConstraint(columnNames = "majorCode"), @UniqueConstraint(columnNames = "majorHead")})
+public class Major extends Audit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private UUID majorCode;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, targetEntity = Lecture.class)
+    @JoinColumn(name = "lecture_id")
+    private Lecture majorHead;
+
+    @Builder.Default
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "major_subject", joinColumns = @JoinColumn(name = "major_id"), inverseJoinColumns = @JoinColumn(name = "subject_id"))
+    private Set<Subject> subjects = new HashSet<>();
+} 
